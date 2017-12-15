6926e15163f17acb8f8125c4f627cefc636069b2
Ethan Bogart <ethan.bogart@yahoo.com>
2017-12-15 19:21:55 +0000

    Remove bad adjectives

--- lib/haikunator.rb
+++ lib/haikunator.rb
@@ -33,9 +33,9 @@ module Haikunator
         icy delicate quiet white cool spring winter patient
         twilight dawn crimson wispy weathered blue billowing
         broken cold damp falling frosty green long late lingering
-        bold little morning muddy old red rough still small
-        sparkling throbbing shy wandering withered wild black
-        young holy solitary fragrant aged snowy proud floral
+        bold little morning muddy old red still small
+        sparkling shy wandering withered wild black
+        young solitary fragrant aged snowy proud floral
         restless divine polished ancient purple lively nameless
       )
     end
